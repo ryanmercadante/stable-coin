e170693b0ddaa5067e32d22f13b22693e42f616c
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2022-07-03 11:22:06 -0400

    use Oracle contract in StableCoin

--- contracts/StableCoin.sol
+++ contracts/StableCoin.sol
@@ -3,28 +3,32 @@ pragma solidity 0.8.11;
 
 import {ERC20} from "./ERC20.sol";
 import {DepositorCoin} from "./DepositorCoin.sol";
+import {Oracle} from "./Oracle.sol";
 
 contract StableCoin is ERC20 {
     DepositorCoin public depositorCoin;
 
-    uint256 private constant ETH_IN_USD_PRICE = 2000;
     uint256 public feeRatePercentage;
+    Oracle public oracle;
 
-    constructor(uint256 _feeRatePercentage) ERC20("StableCoin", "STC") {
+    constructor(uint256 _feeRatePercentage, Oracle _oracle)
+        ERC20("StableCoin", "STC")
+    {
         feeRatePercentage = _feeRatePercentage;
+        oracle = _oracle;
     }
 
     function mint() external payable {
         uint256 fee = _getFee(msg.value);
         uint256 remainingEth = msg.value - fee;
-        uint256 mintStableCoinAmount = remainingEth * ETH_IN_USD_PRICE;
+        uint256 mintStableCoinAmount = remainingEth * oracle.getPrice();
         _mint(msg.sender, mintStableCoinAmount);
     }
 
     function burn(uint256 burnStableCoinAmount) external {
         _burn(msg.sender, burnStableCoinAmount);
 
-        uint256 refundingEth = burnStableCoinAmount / ETH_IN_USD_PRICE;
+        uint256 refundingEth = burnStableCoinAmount / oracle.getPrice();
         uint256 fee = _getFee(refundingEth);
         uint256 remainingRefundingEth = refundingEth - fee;
 
